91694908ab69bbae74cadaa9388d0737bc335dc1
kuavo_CI <kuavo_CI@50_eb_f6_bd_54_c1>
2025-05-14 23:43:01 +0800

    sync https://www.lejuhub.com/highlydynamic/kuavo-ros-control/commit/3cf8d65348593700ace92e09c000792715698901

--- installed/share/hardware_plant/lib/ruierman_controller/python-can-3.3.4/example.run.sh
+++
@@ -1,3 +0,0 @@
-export LD_LIBRARY_PATH=../python-can-3.3.4/can/interfaces/bmcan:$PATH
-export PYTHONPATH=.:../python-can-3.3.4:$PYTHONPATH
-python3 ./examples/receive_all.py
--- installed/share/hardware_plant/lib/ruiwo_controller/python-can-3.3.4/can/interfaces/bmcan/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-
-"""
-"""
-
-from .canlib import BmCanBus
-from .exceptions import BmError
