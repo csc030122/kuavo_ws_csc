af34eb86ec84180cbcc31388f9fa694db748dfae
kuavo_CI <kuavo_CI@50_eb_f6_bd_54_c1>
2025-05-14 20:46:18 +0800

    sync https://www.lejuhub.com/highlydynamic/kuavo-ros-control/commit/3cf8d65348593700ace92e09c000792715698901

--- src/humanoid-control/h12pro_controller_node/src/h12pro_channel_publisher/main.cpp
+++ src/humanoid-control/h12pro_controller_node/src/h12pro_channel_publisher/main.cpp
@@ -22,6 +22,10 @@ void signalHandler(int signum) {
     ros::shutdown();
 }
 
+int limitChannelValue(int value, int min, int max) {
+    return std::max(min, std::min(value, max));
+}
+
 int main(int argc, char **argv) {
     // Register signal handler
     signal(SIGINT, signalHandler);
@@ -43,9 +47,9 @@ int main(int argc, char **argv) {
     while (ros::ok()) {
         recSbusData();
         channel_msg.channels[0] = SbusRxData.channel_1;
-        channel_msg.channels[1] = SbusRxData.channel_2;
+        channel_msg.channels[1] = limitChannelValue(SbusRxData.channel_2, 902, 1102);   // 限制站立、下蹲高度（向上提为902，向下蹲为1102）
         channel_msg.channels[2] = SbusRxData.channel_3;
-        channel_msg.channels[3] = SbusRxData.channel_4;
+        channel_msg.channels[3] = limitChannelValue(SbusRxData.channel_4, 802, 1202);   // 限制左右平移步伐大小（向左走为802，向右走为1202）
         channel_msg.channels[4] = SbusRxData.channel_5;
         channel_msg.channels[5] = SbusRxData.channel_6;
         channel_msg.channels[6] = SbusRxData.channel_7;
