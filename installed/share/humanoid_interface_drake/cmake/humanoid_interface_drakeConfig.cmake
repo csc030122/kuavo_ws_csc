3c147d1c757843e4436df023bb8f5b3129bfc7c6
kuavo_CI <kuavo_CI@50_eb_f6_bd_54_c1>
2026-03-10 10:19:13 +0800

    sync https://www.lejuhub.com/highlydynamic/kuavo-ros-control/commit/09d97ae809bf5aaa5afe6f1f8cd236d0a652f339

--- installed/share/humanoid_interface_drake/cmake/humanoid_interface_drakeConfig.cmake
+++ installed/share/humanoid_interface_drake/cmake/humanoid_interface_drakeConfig.cmake
@@ -67,14 +67,14 @@ set(humanoid_interface_drake_CONFIG_INCLUDED TRUE)
 
 # set variables for source/devel/install prefixes
 if("FALSE" STREQUAL "TRUE")
-  set(humanoid_interface_drake_SOURCE_PREFIX /root/kuavo_ws/builds/tX_ftt_G1/0/highlydynamic/kuavo-ros-control/src/humanoid-control/humanoid_interface_drake)
-  set(humanoid_interface_drake_DEVEL_PREFIX /root/kuavo_ws/builds/tX_ftt_G1/0/highlydynamic/kuavo-ros-control/devel)
+  set(humanoid_interface_drake_SOURCE_PREFIX /media/data/gitlab-runner/builds/8rHoW4Dt/0/highlydynamic/kuavo-ros-control/src/humanoid-control/humanoid_interface_drake)
+  set(humanoid_interface_drake_DEVEL_PREFIX /media/data/gitlab-runner/builds/8rHoW4Dt/0/highlydynamic/kuavo-ros-control/devel)
   set(humanoid_interface_drake_INSTALL_PREFIX "")
   set(humanoid_interface_drake_PREFIX ${humanoid_interface_drake_DEVEL_PREFIX})
 else()
   set(humanoid_interface_drake_SOURCE_PREFIX "")
   set(humanoid_interface_drake_DEVEL_PREFIX "")
-  set(humanoid_interface_drake_INSTALL_PREFIX /root/kuavo_ws/builds/tX_ftt_G1/0/highlydynamic/kuavo-ros-control/installed)
+  set(humanoid_interface_drake_INSTALL_PREFIX /media/data/gitlab-runner/builds/8rHoW4Dt/0/highlydynamic/kuavo-ros-control/installed)
   set(humanoid_interface_drake_PREFIX ${humanoid_interface_drake_INSTALL_PREFIX})
 endif()
 
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /root/kuavo_ws/builds/tX_ftt_G1/0/highlydynamic/kuavo-ros-control/installed/lib;/opt/ros/noetic/lib)
+    foreach(path /media/data/gitlab-runner/builds/8rHoW4Dt/0/highlydynamic/kuavo-ros-control/installed/lib;/opt/ros/noetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
